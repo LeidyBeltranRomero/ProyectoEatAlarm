e7c1ca0bd64744dae92c2d2bb3842435e4860976
LeidyBeltranRomero <lt.beltranr1@uniandes.edu.co>
2023-09-26 11:05:41 -0500

    init-commit

---
+++ app/src/main/java/com/example/proyectoeatalarm/CrearAlarma.kt
@@ -0,0 +1,15 @@
+package com.example.proyectoeatalarm
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class CrearAlarma : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_alarma)
+    }
+
+}
---
+++ app/src/main/java/com/example/proyectoeatalarm/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.proyectoeatalarm
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun abrirCrearAlarma(view: View){
+        val intent = Intent(this,CrearAlarma::class.java).apply {
+            startActivity(intent)
+        }
+    }
+}
