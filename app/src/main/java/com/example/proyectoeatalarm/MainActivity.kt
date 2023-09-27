e4636b8333dcb6b3fee66a2be7b49edbc6754eb2
mauricioobgo <ltbeltranr1@uniandes.edu.co>
2023-09-26 19:02:16 -0500

    initial commit

--- app/src/main/java/com/example/proyectoeatalarm/MainActivity.kt
+++ app/src/main/java/com/example/proyectoeatalarm/MainActivity.kt
@@ -5,12 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
     fun abrirCrearAlarma(view: View){
         val intent = Intent(this,CrearAlarma::class.java).apply {
             startActivity(intent)
--- app/src/main/java/com/example/proyectoeatalarm/ModificarAlarma.kt
+++ app/src/main/java/com/example/proyectoeatalarm/ModificarAlarma.kt
@@ -3,11 +3,10 @@ package com.example.proyectoeatalarm
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+class ModificarAlarma : AppCompatActivity() {
 
-class CrearAlarma : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_crear_alarma)
+        setContentView(R.layout.activity_modificar_alarma)
     }
-
 }
